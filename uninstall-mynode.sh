d612088f64f6e27c13d5ee48c13337dca96e28cc
huangyp <harry@geekworm.com>
2021-09-13 20:12:31 +0800

    support myNode

---
+++ uninstall-mynode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#remove x-c1 old installtion
+sudo sed -i '/x735/d' ~/.bashrc
+sudo sed -i '/x735/d' /etc/rc.local
+sudo sed -i '/pigpiod/d' /etc/rc.local
+sudo sed -i '/python/d' /etc/rc.local
+
+sudo rm /usr/local/bin/x735softsd.sh -f
+sudo rm /etc/x735pwr.sh -f
+
+sudo systemctl disable pigpiod
