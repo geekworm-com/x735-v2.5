461e5f64f271b765f13e4980905369b8f57258cf
Sarah Mischke <mischke@bally-wulff.de>
2021-10-13 10:50:43 +0200

    - remove dependency to pigpiod - refactor pwm_fan_control.py - fix py3 issue with read_fan_speed.py - propper use of python3 for pwm_fan_control.py in all install scripts

--- install.sh
+++ install.sh
@@ -69,15 +69,13 @@ echo "X735 Shutting down..."
 echo "0" > /sys/class/gpio/gpio$BUTTON/value
 ' > /usr/local/bin/x735softsd.sh
 sudo chmod +x /usr/local/bin/x735softsd.sh
-sudo systemctl enable pigpiod
 
 USER_RUN_FILE=/home/pi/.bashrc
 CUR_DIR=$(pwd)
 sudo echo "alias x735off='sudo x735softsd.sh'" >> ${USER_RUN_FILE}
-sudo echo "python ${CUR_DIR}/pwm_fan_control.py&"  >> ${USER_RUN_FILE}
+sudo echo "python3 ${CUR_DIR}/pwm_fan_control.py&"  >> ${USER_RUN_FILE}
 
-sudo pigpiod
-python ${CUR_DIR}/pwm_fan_control.py &
+python3 ${CUR_DIR}/pwm_fan_control.py &
 
 echo "The installation is complete."
 echo "Please run 'sudo reboot' to reboot the device."
--- pwm_fan_control.py
+++ pwm_fan_control.py
@@ -1,35 +1,39 @@
-#!/usr/bin/python
-import pigpio
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import time
+import RPi.GPIO as GPIO
 
-servo = 13
+# configuration
+fan_pin = 13
+temp_pwm_curve = [ (30,30), (50,50), (55,75), (60,90), (80,100) ]
+fan_threshold_temp = 30
 
-pwm = pigpio.pi()
-pwm.set_mode(servo, pigpio.OUTPUT)
-pwm.set_PWM_frequency( servo, 25000 )
-pwm.set_PWM_range(servo, 100)
-while(1):
-     #get CPU temp
-     file = open("/sys/class/thermal/thermal_zone0/temp")
-     temp = float(file.read()) / 1000.00
-     temp = float('%.2f' % temp)
-     file.close()
+# initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(fan_pin, GPIO.OUT)
+pwm = GPIO.PWM(fan_pin, 25000)
+pwm.start(0)
 
-     if(temp > 30):
-          pwm.set_PWM_dutycycle(servo, 40)
+# control loop
+try:
+     while(1):
+          #get CPU temp
+          file = open("/sys/class/thermal/thermal_zone0/temp")
+          temp = float(file.read()) / 1000.00
+          temp = float('%.2f' % temp)
+          file.close()
 
-     if(temp > 50):
-          pwm.set_PWM_dutycycle(servo, 50)
+          if(temp < fan_threshold_temp):
+               pwm.ChangeDutyCycle(0)
+          else:
+               for item in temp_pwm_curve:
+                    if(temp > item[0]):
+                         pwm.ChangeDutyCycle(item[1])
 
-     if(temp > 55):
-          pwm.set_PWM_dutycycle(servo, 75)
+          time.sleep(1)
 
-     if(temp > 60):
-          pwm.set_PWM_dutycycle(servo, 90)
-
-     if(temp > 65):
-          pwm.set_PWM_dutycycle(servo, 100)
-
-     if(temp < 30):
-          pwm.set_PWM_dutycycle(servo, 0)
-     time.sleep(1)
+except:
+    pwm.stop()
+    GPIO.cleanup()
