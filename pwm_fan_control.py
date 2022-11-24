0771385dd384d2a97ecf7aa3275a139701474a0f
huangyp <harry@geekworm.com>
2022-11-24 10:37:22 +0800

    remove pigpiod liberary, use raspberry pi pwm lib

--- pwm_fan_control.py
+++ pwm_fan_control.py
@@ -1,35 +1,38 @@
-#!/usr/bin/python
-import pigpio
+#!/usr/bin/python3
+
+import RPi.GPIO as IO
 import time
+import subprocess
 
 servo = 13
+IO.setwarnings(False)
+IO.setmode (IO.BCM)
+IO.setup(servo,IO.OUT)
+fan = IO.PWM(servo,25000)
+fan.start(0)
 
-pwm = pigpio.pi()
-pwm.set_mode(servo, pigpio.OUTPUT)
-pwm.set_PWM_frequency( servo, 25000 )
-pwm.set_PWM_range(servo, 100)
-while(1):
-     #get CPU temp
-     file = open("/sys/class/thermal/thermal_zone0/temp")
-     temp = float(file.read()) / 1000.00
-     temp = float('%.2f' % temp)
-     file.close()
+def get_temp():
+    output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
+    temp_str = output.stdout.decode()
+    try:
+        return float(temp_str.split('=')[1].split('\'')[0])
+    except (IndexError, ValueError):
+        raise RuntimeError('Could not get temperature')
 
-     if(temp > 30):
-          pwm.set_PWM_dutycycle(servo, 40)
-
-     if(temp > 50):
-          pwm.set_PWM_dutycycle(servo, 50)
-
-     if(temp > 55):
-          pwm.set_PWM_dutycycle(servo, 75)
-
-     if(temp > 60):
-          pwm.set_PWM_dutycycle(servo, 90)
-
-     if(temp > 65):
-          pwm.set_PWM_dutycycle(servo, 100)
-
-     if(temp < 30):
-          pwm.set_PWM_dutycycle(servo, 0)
-     time.sleep(1)
+while 1:
+    temp = get_temp()                        # Get the current CPU temperature
+    if temp > 70:                            # Check temperature threshhold, in degrees celcius
+        fan.ChangeDutyCycle(100)             # Set fan duty based on temperature, 100 is max speed and 0 is min speed or off.
+    elif temp > 60:
+        fan.ChangeDutyCycle(85)
+    elif temp > 50:
+        fan.ChangeDutyCycle(70)
+    elif temp > 40:
+        fan.ChangeDutyCycle(50)
+    elif temp > 32:
+        fan.ChangeDutyCycle(25)
+    elif temp > 25:
+        fan.ChangeDutyCycle(15)
+    else:
+        fan.ChangeDutyCycle(0)
+    time.sleep(5)                            # Sleep for 5 seconds
