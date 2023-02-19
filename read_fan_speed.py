9d18ffb345441b1c1d0c45f69257b98b0adc36ff
Molham Al Nasr <dr.molham93@gmail.com>
2023-02-19 20:22:38 +0100

    create a class to encapsulate the state of the sensor

--- read_fan_speed.py
+++ read_fan_speed.py
@@ -11,35 +11,42 @@ WAIT_TIME = 1
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(TACH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+class RPMCounter:
+    def __init__(self, pin):
+        self.pin = pin
+        self.rpm = 0
+        self.last_time = time.time()
 
-t = time.time()
-rpm = 0
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, self._update_rpm)
 
-def fell(n):
-    global t
-    global rpm
+    def _update_rpm(self, channel):
+        dt = time.time() - self.last_time
+        if dt < 0.005:
+            return
 
-    dt = time.time() - t
-    if dt < 0.005: return
+        freq = 1 / dt
+        self.rpm = (freq / PULSE) * 60
+        self.last_time = time.time()
 
-    freq = 1 / dt
-    rpm = (freq / PULSE) * 60
-    t = time.time()
+    def get_rpm(self):
+        current_rpm = self.rpm
+        self.rpm = 0
+        return current_rpm
 
-GPIO.add_event_detect(TACH, GPIO.FALLING, fell)
+if __name__ == '__main__':
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        rpm_counter = RPMCounter(TACH)
 
-try:
-    while True:
-        # print "%.f RPM" % rpm
-        print("%.f RPM" % rpm)
-        rpm = 0
-        time.sleep(WAIT_TIME)
+        while True:
+            rpm = rpm_counter.get_rpm()
+            logging.info('Current RPM: {:.2f}'.format(rpm))
+            time.sleep(WAIT_TIME)
 
-except KeyboardInterrupt:
-    logging.info('Exiting...')
-finally:
-    # Clean up the GPIO resources
-    GPIO.cleanup()
+    except KeyboardInterrupt:
+        logging.info('Exiting...')
+    finally:
+        # Clean up the GPIO resources
+        GPIO.cleanup()
