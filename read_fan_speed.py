ea2b10409d5083737432944056cb35a1a5cacc5e
Molham Al Nasr <dr.molham93@gmail.com>
2023-02-19 20:25:23 +0100

    Add comments to the code to make it more readable and easier to understand

--- read_fan_speed.py
+++ read_fan_speed.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
 import RPi.GPIO as GPIO
 import time
 import logging
@@ -17,10 +17,12 @@ class RPMCounter:
         self.rpm = 0
         self.last_time = time.time()
 
+        # Set up the GPIO pin for input with a pull-up resistor
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(self.pin, GPIO.FALLING, self._update_rpm)
 
     def _update_rpm(self, channel):
+        # Calculate the RPM based on the time between pulses
         dt = time.time() - self.last_time
         if dt < 0.005:
             return
@@ -30,16 +32,19 @@ class RPMCounter:
         self.last_time = time.time()
 
     def get_rpm(self):
+        # Get the current RPM and reset the counter
         current_rpm = self.rpm
         self.rpm = 0
         return current_rpm
 
 if __name__ == '__main__':
     try:
+        # Set up the GPIO and RPMCounter object
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         rpm_counter = RPMCounter(TACH)
 
+        # Loop indefinitely, printing the RPM every second
         while True:
             rpm = rpm_counter.get_rpm()
             logging.info('Current RPM: {:.2f}'.format(rpm))
