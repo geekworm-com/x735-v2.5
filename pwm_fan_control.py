70cc92abc82d63fa7d4df2487763241f3bad5dd3
huangyp <harry@geekworm.com>
2023-03-04 14:51:29 +0800

    Modify the script to improve CPU load occupancy

--- pwm_fan_control.py
+++ pwm_fan_control.py
@@ -8,7 +8,7 @@ servo = 13
 IO.setwarnings(False)
 IO.setmode (IO.BCM)
 IO.setup(servo,IO.OUT)
-fan = IO.PWM(servo,25000)
+fan = IO.PWM(servo,200)
 fan.start(0)
 
 def get_temp():
@@ -18,7 +18,6 @@ def get_temp():
         return float(temp_str.split('=')[1].split('\'')[0])
     except (IndexError, ValueError):
         raise RuntimeError('Could not get temperature')
-
 while 1:
     temp = get_temp()                        # Get the current CPU temperature
     if temp > 70:                            # Check temperature threshhold, in degrees celcius
@@ -26,13 +25,13 @@ while 1:
     elif temp > 60:
         fan.ChangeDutyCycle(85)
     elif temp > 50:
-        fan.ChangeDutyCycle(70)
+        fan.ChangeDutyCycle(60)
     elif temp > 40:
         fan.ChangeDutyCycle(50)
     elif temp > 32:
-        fan.ChangeDutyCycle(25)
+        fan.ChangeDutyCycle(45)
     elif temp > 25:
-        fan.ChangeDutyCycle(15)
+        fan.ChangeDutyCycle(40)
     else:
         fan.ChangeDutyCycle(0)
     time.sleep(5)                            # Sleep for 5 seconds
